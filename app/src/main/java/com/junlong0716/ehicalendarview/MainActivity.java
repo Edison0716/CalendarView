5ff47a034bc6810b69584cb9db022cc32358724f
EdisonLi <18644056528@163.com>
2019-05-16 15:59:31 +0800

    2019-05-15 完善点击样式

--- app/src/main/java/com/junlong0716/ehicalendarview/MainActivity.java
+++ app/src/main/java/com/junlong0716/ehicalendarview/MainActivity.java
@@ -2,6 +2,9 @@ package com.junlong0716.ehicalendarview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.junlong0716.pickerview.MonthView;
@@ -16,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MonthView mv = findViewById(R.id.mv);
-        mv.setCalendarParams(2019,1, Calendar.SUNDAY);
+        mv.setCalendarParams(2019, 6, getBitmap());
+    }
+
+    private Bitmap getBitmap() {
+        Drawable drawable = getResources().getDrawable(R.drawable.shape_selected);
+        Canvas canvas = new Canvas();
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        canvas.setBitmap(bitmap);
+        drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+        drawable.draw(canvas);
+        return bitmap;
     }
 }
--- lib_calendar_picker/src/main/java/com/junlong0716/pickerview/MonthView.java
+++ lib_calendar_picker/src/main/java/com/junlong0716/pickerview/MonthView.java
@@ -1,11 +1,16 @@
 package com.junlong0716.pickerview;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Typeface;
 import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -16,15 +21,20 @@ public class MonthView extends View {
     private Context mContext;
     //文字画笔
     private TextPaint mTextPaint;
+    //文字选中颜色
+    private TextPaint mTextSelectedPaint;
+    //描述文字画笔
+    private TextPaint mDesTextPaint;
+    private Paint mSelectedPaint;
     //一星期7天
     private int mDaysInWeek = 7;
     //一个月最大行数
-    private int mMaxWeeksInMonth = 6;
+    private int mMaxWeeksLinesInMonth = 6;
     //一个月多少天
     private int mDaysInMonth = 0;
-    //列间距
+    //一行去掉内间距的宽
     private int mPaddedWidth = 0;
-    //行间距
+    //一行去掉内间距的高
     private int mPaddedHeight = 0;
     //一个格子的宽高
     private int mCellWith;
@@ -37,6 +47,9 @@ public class MonthView extends View {
     private int mYear = 0;
     private int mMonth = 0;
     private Calendar mCalendar = Calendar.getInstance();
+    //选中的日期
+    private int mSelectDay = 0;
+    private Bitmap mSelectBitmap;
 
     public MonthView(Context context) {
         super(context);
@@ -60,53 +73,65 @@ public class MonthView extends View {
         mTextPaint = new TextPaint();
         mTextPaint.setAntiAlias(true);
         mTextPaint.setColor(Color.parseColor("#333333"));
-        mTextPaint.setTextSize(ConvertUtil.dp2px(15f, mContext));
+        Typeface boldFont = Typeface.create(Typeface.SANS_SERIF, Typeface.BOLD);
+        mTextPaint.setTypeface(boldFont);
+        mTextPaint.setTextSize(ConvertUtil.dp2px(16f, mContext));
         mTextPaint.setTextAlign(Paint.Align.CENTER);
+
+        mDesTextPaint = new TextPaint();
+        mDesTextPaint.setAntiAlias(true);
+        mDesTextPaint.setColor(Color.parseColor("#FF7E00"));
+        mDesTextPaint.setTextSize(ConvertUtil.dp2px(12f, mContext));
+        mDesTextPaint.setTextAlign(Paint.Align.CENTER);
+
+        mSelectedPaint = new Paint();
+        mSelectedPaint.setFilterBitmap(true);
+        mSelectedPaint.setAntiAlias(true);
+        mSelectedPaint.setDither(true);
+        mSelectedPaint.setColor(Color.parseColor("#29B7B7"));
+
+        mTextSelectedPaint = new TextPaint();
+        mTextSelectedPaint.setColor(Color.WHITE);
+        mTextSelectedPaint.setAntiAlias(true);
+        mTextSelectedPaint.setTypeface(boldFont);
+        mTextSelectedPaint.setTextSize(ConvertUtil.dp2px(16f, mContext));
+        mTextSelectedPaint.setTextAlign(Paint.Align.CENTER);
+
         mCalendar.setFirstDayOfWeek(Calendar.SUNDAY);
     }
 
-    public void setCalendarParams(int year, int month, int weekStart) {
+    public void setCalendarParams(int year, int month, Bitmap selectBitmap) {
+        mSelectBitmap = selectBitmap;
         if (isValidMonth(month - 1))
             mMonth = month - 1;
         mYear = year;
-        mCalendar.set(Calendar.YEAR, year);
-        mCalendar.set(Calendar.MONTH, month);
+        mCalendar.set(Calendar.YEAR, mYear);
+        mCalendar.set(Calendar.MONTH, mMonth);
         mCalendar.set(Calendar.DAY_OF_MONTH, 1);
-        mDaysInMonth = getDaysInMonth(year, month);
+        mDaysInMonth = getDaysInMonth(mYear, mMonth);
         mDayOfWeekStart = mCalendar.get(Calendar.DAY_OF_WEEK);
-        if (isValidDay(weekStart))
-            mWeekStart = weekStart;
-        else
-            mWeekStart = mCalendar.getFirstDayOfWeek();
+        mWeekStart = mCalendar.getFirstDayOfWeek();
+
+        //判断最大行数
+        if (mDayOfWeekStart == 6 && mDaysInMonth == 31)
+            mMaxWeeksLinesInMonth = 6;
+        else if (mDayOfWeekStart == 7 && mDaysInMonth >= 30)
+            mMaxWeeksLinesInMonth = 6;
+        else mMaxWeeksLinesInMonth = 5;
         requestLayout();
     }
 
     private int getDaysInMonth(int year, int month) {
-        int days = 0;
-
-        switch (month) {
-            case Calendar.JANUARY:
-            case Calendar.MARCH:
-            case Calendar.MAY:
-            case Calendar.JULY:
-            case Calendar.AUGUST:
-            case Calendar.DECEMBER:
-            case Calendar.OCTOBER:
-                days = 31;
-                break;
-            case Calendar.JUNE:
-            case Calendar.SEPTEMBER:
-            case Calendar.NOVEMBER:
-            case Calendar.APRIL:
-                days = 30;
-                break;
-            case Calendar.FEBRUARY:
-                if (year % 4 == 0)
-                    days = 29;
-                else
-                    days = 28;
-                break;
-        }
+        int days;
+        if (month == Calendar.FEBRUARY) {
+            if (year % 4 == 0)
+                days = 29;
+            else
+                days = 28;
+        } else if (month == Calendar.JUNE || month == Calendar.SEPTEMBER || month == Calendar.NOVEMBER || month == Calendar.APRIL)
+            days = 30;
+        else
+            days = 31;
         return days;
     }
 
@@ -116,9 +141,38 @@ public class MonthView extends View {
         mCellWith = (MeasureSpec.getSize(widthMeasureSpec) - getPaddingLeft() - getPaddingRight()) / mDaysInWeek;
         mCellHeight = mCellWith;
         //目标高度
-        int targetHeight = mCellHeight * mMaxWeeksInMonth + getPaddingTop() + getPaddingBottom() + mMonthHeight;
+        int targetHeight = mCellHeight * mMaxWeeksLinesInMonth + getPaddingTop() + getPaddingBottom() + mMonthHeight;
         //测量
         setMeasuredDimension(widthMeasureSpec, resolveSize(targetHeight, heightMeasureSpec));
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int x = (int) (event.getX() + 0.5f);
+        int y = (int) (event.getY() + 0.5f);
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mSelectDay = getDaysLocation(x, y);
+                Log.d("mSelectDay", mSelectDay + "");
+                break;
+        }
+        invalidate();
+        return true;
+    }
+
+    //根据位置信息找到选中的天数
+    private int getDaysLocation(int x, int y) {
+        int paddedX = x - getPaddingLeft();
+        if (paddedX < 0 || paddedX >= mPaddedWidth) return -1;
+        int paddedY = y - getPaddingTop();
+        if (paddedY < mMonthHeight || paddedY >= mPaddedHeight) return -1;
+        int row = (paddedY - mMonthHeight) / mCellHeight; //行高 点击位置所在的第几行
+        int col = paddedX * mDaysInWeek / mPaddedWidth; //列宽 点击位置所在的第几列
+        int index = col + row * mDaysInWeek;//点击在第几个格子 一行7个数字 行数 * 7 + 列数 = 日期天
+        int selectDay = index + 1 - getDayOffset();//这里应该去掉之前的空格
+        Log.d("selectedDay", selectDay + "");
+        if (isValidDay(selectDay)) return selectDay;
+        else return -1;
     }
 
     @Override
@@ -166,16 +220,24 @@ public class MonthView extends View {
             int cellStartY = rowHeightCenter - rowHeight / 2;
             //一个格子的中点横坐标
             int colCenter = cellStartX + colWidth / 2;
-
-            //todo 点击高亮
             Calendar calendar = Calendar.getInstance();
             calendar.set(Calendar.YEAR, mYear);
             calendar.set(Calendar.MONTH, mMonth);
             calendar.set(Calendar.DAY_OF_MONTH, day);
+            //高亮
+            if (mSelectDay == day)
+                drawSelectedTextLightBg(canvas, (float) colCenter, rowHeightCenter);
 
-            drawText(canvas, String.valueOf(day), (float) colCenter, rowHeightCenter - halfTextLineHeight,
-                    calendar, mTextPaint);
+            if (mSelectDay == day){
+                drawSelectedText(canvas, String.valueOf(day), (float) colCenter, rowHeightCenter - halfTextLineHeight - ConvertUtil.dp2px(8, mContext));
+                drawSelectedDesText(canvas, "￥155", (float) colCenter, rowHeightCenter + halfTextLineHeight + ConvertUtil.dp2px(20, mContext));
+            }
+            else{
+                drawDesText(canvas, "￥155", (float) colCenter, rowHeightCenter + halfTextLineHeight + ConvertUtil.dp2px(20, mContext));
+                drawText(canvas, String.valueOf(day), (float) colCenter, rowHeightCenter - halfTextLineHeight - ConvertUtil.dp2px(8, mContext));
+            }
 
+            //绘制到了最后一个格子 则从头开始
             if (++colOffset == mDaysInWeek) {
                 colOffset = 0;
                 rowHeightCenter += rowHeight;
@@ -183,9 +245,29 @@ public class MonthView extends View {
         }
     }
 
-    private void drawText(Canvas canvas, String dayString, float x, float y, Calendar calendar, Paint paint) {
+
+    private void drawSelectedDesText(Canvas canvas, String des, float x, float y) {
+        mDesTextPaint.setColor(Color.WHITE);
+        canvas.drawText(des, x, y, mDesTextPaint);
+    }
+
+    private void drawSelectedText(Canvas canvas, String dayString, float x, float y) {
+        canvas.drawText(dayString, x, y, mTextSelectedPaint);
+    }
+
+    private void drawSelectedTextLightBg(Canvas canvas, float x, float y) {
+        if (mSelectBitmap == null) return;
+        canvas.drawRoundRect(new RectF(x - 60, y - 60, x + 60, y + 60), 8, 8, mSelectedPaint);
+    }
+
+    private void drawDesText(Canvas canvas, String des, float x, float y) {
+        mDesTextPaint.setColor(Color.parseColor("#FF7E00"));
+        canvas.drawText(des, x, y, mDesTextPaint);
+    }
+
+    private void drawText(Canvas canvas, String dayString, float x, float y) {
         //todo 判断昨天 变灰
-        canvas.drawText(dayString, x, y, paint);
+        canvas.drawText(dayString, x, y, mTextPaint);
     }
 
     private boolean isValidMonth(int month) {
@@ -193,8 +275,9 @@ public class MonthView extends View {
     }
 
     private boolean isValidDay(int day) {
-        return day >= Calendar.SUNDAY && day <= Calendar.SATURDAY;
+        return day >= 1 && day <= 31;
     }
+
 
     //计算月份第一个格子的偏移量
     private int getDayOffset() {
